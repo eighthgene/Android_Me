15f4ba36e475c9151c85af34316e7ac55b6b6d32
Oleg Sokolov <olegsokolov91@gmail.com>
2018-07-17 18:53:17 +0200

    finish all todo ex.4

--- app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
 
 import com.example.android.android_me.R;
 
@@ -17,7 +19,10 @@ public class MasterListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_master_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        GridView gridView =  view.findViewById(R.id.images_grid_view);
+        return null;
 
     }
 }

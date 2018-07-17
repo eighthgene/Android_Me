8b0be261554570e6772c7cfcf5eaf4ee1262d08b
Oleg Sokolov <olegsokolov91@gmail.com>
2018-07-17 18:48:55 +0200

    finish all todo ex.4

---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,23 @@
+package com.example.android.android_me.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.android_me.R;
+
+public class MasterListFragment extends Fragment {
+    public MasterListFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_master_list, container, false);
+
+    }
+}
